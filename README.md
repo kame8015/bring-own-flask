32905556a4ef53f655fdf1f04ecd26661b201164
亀田真吾 <kame@kamedashingonoMacBook-ea.local>
2021-06-01 22:15:49 +0900

    implement sample

--- README.md
+++ README.md
@@ -1,1 +1,101 @@
-# bring-own-flask
+# Container 挙動確認
+
+`this is model!!`と書かれた
+`/opt/ml/model/sample.txt`  
+の末尾に、`this is input!!`と書かれた
+`/opt/ml/input/input.txt`の文字列を追記して、
+`/opt/ml/output/predicted.txt`  
+としてインメモリに保存する。
+
+1. `predictor.py`の`predict`メソッドで読み込んでいるインプットファイルの名前を`input.txt`に修正する
+1. Docker イメージを作成する
+   ```
+   $ cd container
+   $ docker build -t image_test .
+   ```
+1. Docker コンテナを立ち上げて bash に入る(使用したコンテナは終了時に自動削除)
+   ```
+   $ docker run -it --rm image_test /bin/bash
+   ```
+1. コンテナ内ボリュームの`/opt/ml/model`に`sample.txt`がいることを確認する
+   ```
+   root@dda9409b7c43:/opt/program# cat ../ml/model/sample.txt
+   this is model!!
+   ```
+1. コンテナ内ボリュームの`/opt/ml/input`に`input.txt`がいることを確認する
+   ```
+   root@dda9409b7c43:/opt/program# cat ../ml/input/input.txt
+   this is input!!
+   ```
+1. ローカルでコンテナを起動して nginx サーバを立ち上げる
+   ```
+   $ docker run --name container_test -p 8080:8080 image_test serve
+   ```
+1. nginx サーバに ping を送り死活管理を行う
+   ```
+   $ curl http://localhost:8080/ping
+   ```
+1. nginx サーバに実行をリクエストする
+   ```
+   $ curl -X POST http://localhost:8080/invocations
+   this is model!!this is input!!
+   ```
+1. nginx サーバを Cmd+C で終了する
+1. ローカルでコンテナを起動して bash に入り、`opt/ml/output/predicted.txt`があることを確認する
+   ```
+   $ docker start container_test
+   $ docker exec -it container_test /bin/bash
+   root@defa1a5a3d0d:/opt/program# cat ../ml/output/predicted.txt
+   this is model!!this is input!!
+   ```
+
+# SageMaker での挙動確認
+
+S3 の`kame-sagemaker-txt`バケットの
+`/model/model.tar.gz`を読み込み、
+`/predict/input/input.txt`を使って、
+`/predict/output/input.txt.out`を作り出す
+
+1. `~/.aws/credentials`に アクセスキー ID と シークレット を書いておく
+   ```
+   $ aws configure --profile {profile_name}
+   AWS Access Key ID: ********************
+   AWS Secret Access Key: ********************
+   Default region name [None]: ap-northeast-1
+   Default output format [None]: json
+   ```
+1. `txt_test` という Docker イメージを作成して ECR にプッシュする
+   ```
+   $ cd container
+   $ ./build_and_push.sh txt_test {profile_name}
+   $ cd ../
+   ```
+1. ECR の画面でイメージが登録されていることを確認する
+1. S3 に bucket とフォルダを作成する
+   - 親となる bucket: kame-sagemaker-txt(以後はこの下に作成)
+   - モデルを格納しておくフォルダ: `/model`
+   - 推論処理に使用されるインプットフォルダ: `/predict/input`
+   - 推論後のアウトプットフォルダ: `/predict/output`
+1. S3 にモデル、インプットファイルをアップロードする
+   - モデル: `/model/model.tar.gz` (中身は`sample.txt`)
+   - インプットファイル: `/predict/input/sagemaker_input.txt`
+1. 仮想環境を作って `requirements.txt` を pip install する
+   ```
+   $ python -m venv .venv
+   $ source .venv/bin/activate
+   $ pip install -r requirements.txt
+   ```
+1. ECR に登録されたイメージと S3 にアップロードされたモデルファイルを使用して、SageMaker のモデルをデプロイする
+   ```
+   $ python sagemaker_deploy.py
+   ```
+1. SageMaker の「モデル」に新しく作成されているのを確認する
+1. SageMaker にデプロイされたモデルを使用して、バッチ推論ジョブを起動する
+   ```
+   $ python sagemaker_predict.py
+   ```
+1. SageMaker の「バッチ変換ジョブ」に新しいジョブが作成されていることを確認する
+1. 終わったら、S3 の`kame-sagemaker-txt`バケットに`/predict/output/sagemaker_input.txt.out`が出力されていることを確認する
+   ```
+   this is model!!SageMaker input File!!
+   ```
---
+++ container/predict/predictor.py
@@ -0,0 +1,106 @@
+from __future__ import print_function
+import os
+from PIL import Image, ImageOps
+import io
+import flask
+import base64
+
+# The flask app for serving predictions
+app = flask.Flask(__name__)
+
+prefix = "/opt/ml/"
+model_path = os.path.join(prefix, "model")
+input_path = os.path.join(prefix, "input")
+output_path = os.path.join(prefix, "output")
+
+UPLOAD_FOLDER = "./uploads"
+ALLOWED_EXTENTIONS = set(["jpg", "jpeg"])
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+
+class ScoringService(object):
+    model = None  # Where we keep the model when it's loaded
+
+    @classmethod
+    def allowed_file(cls, filename):
+        # 許可された拡張子だと1、でなければ0を返す
+        return (
+            "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENTIONS
+        )
+
+    @classmethod
+    def get_model(cls):
+        """Get the model object for this instance, loading it if it's not already loaded."""
+        # if cls.model == None:
+        #     with open(os.path.join(model_path, "decision-tree-model.pkl"), "rb") as inp:
+        #         cls.model = pickle.load(inp)
+        # return cls.model
+
+        if cls.model == None:
+            with open(os.path.join(model_path, "model.txt"), encoding="utf-8") as file:
+                cls.model = file.read()
+        return cls.model
+
+    @classmethod
+    def predict(cls, input):
+        """For the input, do the predictions and return them.
+
+        Args:
+            input (a pandas dataframe): The data on which to do the predictions. There will be
+                one prediction per row in the dataframe"""
+        text = cls.get_model()
+        text += input
+
+        # ここで detectionCrackPy に画像パスを渡す
+        return text
+
+
+@app.route("/ping", methods=["GET"])
+def ping():
+    """Determine if the container is working and healthy. In this sample container, we declare
+    it healthy if we can load the model successfully."""
+    health = ScoringService.get_model() is not None
+
+    status = 200 if health else 404
+    return flask.Response(response="\n", status=status, mimetype="application/json")
+
+
+@app.route("/invocations", methods=["POST"])
+def transformation():
+    """Do an inference on a single batch of data. In this sample server, we take data as CSV, convert
+    it to a pandas data frame for internal use and then convert the predictions back to CSV (which really
+    just means one prediction per line, since there's a single column.
+    """
+    data = None
+
+    # txt/plain ファイルを読み込み．バイナリから文字列に変換
+    # data = flask.request.get_data().decode("utf-8")
+
+    # データの取り出し
+    if flask.request.content_type == "application/x-image":
+        print("reading input file...")
+        img_binary = flask.request.data
+        img = Image.open(io.BytesIO(img_binary)).convert("RGB")
+        print(img.size)
+        # img_invert = ImageOps.invert(img)
+
+        # img_invert.save(os.path.join(output_path, "output.jpg"), format="jpeg")
+
+    # Do the prediction
+    # predictions = ScoringService.predict(data)
+
+    # /opt/ml/output に書き出し
+    # with open(os.path.join(output_path, "predicted.txt"), mode="w") as f:
+    #     print("writing...")
+    #     f.write(predictions)
+
+    # result = open(os.path.join(output_path, "predicted.txt"))
+
+    # response = flask.make_response(img_binary)
+    response = flask.make_response(img_binary)
+    response.headers.set("Content-Type", "image/jpeg")
+
+    print("Inverted!!!!!")
+
+    # return flask.Response(response=response, status=200)
+    return response
